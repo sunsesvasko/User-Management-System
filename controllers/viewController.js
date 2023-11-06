f14531828d900d9efc8c7b170726201402499fa6
Vasko <vbblajev@gmail.com>
2023-11-06 20:33:43 +0200

    Create User & Update User design & route setup done

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,4 +1,17 @@
 exports.getOverview = (req, res) => {
+    res.status(200).render('users', {
+        title: 'User Management System'
+    });
+}
 
-    res.status(200).render('users');
+exports.getNewUserForm = (req, res) => {
+    res.status(200).render('newUser', {
+        title: 'Create New User'
+    });
+}
+
+exports.getUpdateUserForm = (req, res) => {
+    res.status(200).render('updateUser', {
+        title: 'Update a User'
+    });
 }
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,5 +4,7 @@ const viewController = require('./../controllers/viewController');
 const router = express.Router();
 
 router.get('/', viewController.getOverview);
+router.get('/newUser', viewController.getNewUserForm);
+router.get('/updateUser', viewController.getUpdateUserForm)
 
 module.exports = router;
